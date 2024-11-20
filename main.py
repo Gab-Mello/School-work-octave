fc1601209365850a265d4a1b4e448bd9e5cfc18e
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-11-20 20:29:19 -0300

    configurations

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from controllers import octaveController
-app = FastAPI(
-    title="API de Geração de PDFs",
-    description="API para geração de arquivos PDF",
-    version="1.0.0"
-)
+import uvicorn
+
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(octaveController.router)
@@ -21,5 +19,4 @@ app.add_middleware(
 )
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
